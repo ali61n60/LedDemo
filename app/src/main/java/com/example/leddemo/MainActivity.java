c6614e8ff29d2945eb25e8d87bbba53c142c92cf
Ali <ali62n62@yahoo.com>
2020-03-18 19:11:15 +0330

    Initial commit

---
+++ app/src/main/java/com/example/leddemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.leddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FlashlightProvider flashlightProvider;
+    boolean isFlashOn = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(this);
+        flashlightProvider = new FlashlightProvider(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (isFlashOn) {
+            flashlightProvider.turnFlashlightOff();
+            isFlashOn = false;
+        } else {
+            flashlightProvider.turnFlashlightOn();
+            isFlashOn = true;
+        }
+    }
+}
