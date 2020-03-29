804ddb02ab0c810f2ef8ff561e4abc6a1b976571
Ali <ali62n62@yahoo.com>
2020-03-29 11:33:02 +0430

    importing xamarin demo projects

--- app/src/main/java/com/example/leddemo/MainActivity.java
+++ app/src/main/java/com/example/leddemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        changeLocaleTo("fa");
+       // changeLocaleTo("fa");
 
         setContentView(R.layout.activity_main);
 
